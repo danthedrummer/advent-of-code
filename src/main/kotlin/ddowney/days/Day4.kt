9544995bfdeb69dd1c267af7619066f97f6b25f1
Dan Downey <ddowney@squarespace.com>
2022-12-12 11:53:53 +0000

    Added 2021 solutions for day 4 and 5

---
+++ src/main/kotlin/ddowney/days/Day4.kt
@@ -0,0 +1,97 @@
+package ddowney.days
+
+import ddowney.Day
+
+internal class Day4 : Day("day-4-input.txt") {
+  override fun part1(): String {
+    val numbers = input.first().split(",").map { it.toInt() }
+    val boards = parseBoards()
+
+    numbers.forEach { number ->
+      boards.forEach { board ->
+        if (board.mark(number)) {
+          return "Bingo total = ${board.total * number}"
+        }
+      }
+    }
+    return "No winner :("
+  }
+
+  override fun part2(): String {
+    val numbers = input.first().split(",").map { it.toInt() }
+    val boards = parseBoards()
+
+    numbers.forEach { number ->
+      val winners = mutableListOf<BingoBoard>()
+      boards.forEach { board ->
+        if (board.mark(number)) {
+          if (boards.size == 1) {
+            return "Last winner total = ${board.total * number}"
+          }
+          winners.add(board)
+        }
+      }
+      boards.removeAll(winners)
+    }
+    return "No winner :("
+  }
+
+  private fun parseBoards(): MutableList<BingoBoard> {
+    val boards = mutableListOf<BingoBoard>()
+    val digits = mutableListOf<Int>()
+    input.subList(2, input.size).forEach { line ->
+      if (line.isNotEmpty()) {
+        digits.addAll(line.split(" ").filter { it.isNotEmpty() }.map { it.toInt() })
+      }
+      if (digits.size == 25) {
+        boards.add(BingoBoard(digits.toMutableList()))
+        digits.clear()
+      }
+    }
+    return boards
+  }
+
+}
+
+data class BingoBoard(
+  private val numbers: MutableList<Int>,
+) {
+
+  var total = numbers.sum()
+
+  fun mark(number: Int): Boolean {
+    println("Calling number $number")
+    if (numbers.contains(number)) {
+      numbers[numbers.indexOf(number)] = -1
+      total -= number
+    }
+    return checkForBingo()
+  }
+
+  private fun checkForBingo(): Boolean {
+    println(toString())
+    for (i in 0 until 5) {
+      var bingo = true
+      for (j in 0 until 5) {
+        if (numbers[(i * 5) + j] != -1) {
+          bingo = false
+        }
+      }
+      if (bingo) {
+        return true
+      }
+    }
+    for (i in 0 until 5) {
+      var bingo = true
+      for (j in 0 until 5) {
+        if (numbers[i + (j * 5)] != -1) {
+          bingo = false
+        }
+      }
+      if (bingo) {
+        return true
+      }
+    }
+    return false
+  }
+}
---
+++ src/main/kotlin/ddowney/days/Day5.kt
@@ -0,0 +1,97 @@
+package ddowney.days
+
+import ddowney.Day
+import java.lang.Integer.min
+import kotlin.math.abs
+import kotlin.math.max
+
+internal class Day5 : Day("day-5-input.txt") {
+
+  override fun part1(): String {
+    val grid: MutableList<MutableList<Int>> = MutableList(1000) {
+      MutableList(1000) { 0 }
+    }
+    val lines = parseData()
+
+    lines.forEach { line ->
+      if (line.start.y == line.end.y) {
+        val begin = min(line.start.x, line.end.x)
+        val end = max(line.start.x, line.end.x)
+        for (i in begin..end) {
+          grid[line.start.y][i] += 1
+        }
+      } else if (line.start.x == line.end.x) {
+        val begin = min(line.start.y, line.end.y)
+        val end = max(line.start.y, line.end.y)
+        for (i in begin..end) {
+          grid[i][line.start.x] += 1
+        }
+      } else {
+        // Ignoring diagonals
+      }
+    }
+
+    return "Number of straight line overlaps = ${grid.flatten().count { it >= 2 }}"
+  }
+
+  override fun part2(): String {
+    val gridSize = 1000
+    val grid: MutableList<MutableList<Int>> = MutableList(gridSize) {
+      MutableList(gridSize) { 0 }
+    }
+    val lines = parseData()
+
+    lines.forEach { line ->
+      val xDiff = line.end.x - line.start.x
+      val yDiff = line.end.y - line.start.y
+      val xTransform = calculateTransform(xDiff)
+      val yTransform = calculateTransform(yDiff)
+      val steps = max(abs(xDiff), abs(yDiff))
+      var i = line.start.x
+      var j = line.start.y
+      grid[j][i] += 1
+      repeat(steps) {
+        grid[j + yTransform][i + xTransform] += 1
+        i += xTransform
+        j += yTransform
+      }
+    }
+
+    if (gridSize <= 10) {
+      // For debugging against a smaller data set
+      displayGrid(grid)
+    }
+
+    return "Number of overlaps = ${grid.flatten().count { it >= 2 }}"
+  }
+
+  private fun parseData(): List<Line> = input.map { entry ->
+    entry.split(" -> ").map { lines ->
+      val (x, y) = lines.split(",").map { it.toInt() }
+      Point(x, y)
+    }.let { points ->
+      val (start, end) = points
+      Line(start, end)
+    }
+  }
+
+  private fun calculateTransform(diff: Int): Int = if (diff > 0) {
+    1
+  } else if (diff < 0) {
+    -1
+  } else {
+    0
+  }
+
+  private fun displayGrid(grid: List<List<Int>>) {
+    grid.forEach { row ->
+      row.forEach { count -> print("$count, ") }
+      println()
+    }
+  }
+
+}
+
+data class Point(val x: Int, val y: Int)
+
+data class Line(val start: Point, val end: Point)
